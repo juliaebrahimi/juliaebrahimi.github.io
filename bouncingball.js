2d00580f5eb3943d82094a7baad437aa868efdaf
juliaebrahimi <62028563+juliaebrahimi@users.noreply.github.com>
2020-04-28 15:19:35 -0700

    Update bouncingball.js
    Added a move function

--- bouncingball.js
+++ bouncingball.js
@@ -3,39 +3,37 @@ var y = 50;
 var size = 100;
 var dirY = 1;
 var dirX = 1;
+var r = 141;
+var g = 161;
+var b = 220;
 
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(400, 400);
 }
 
 function draw() {
-    background(164, 26, 74);
-    ellipse(x, y, size);
-    fill(141, 161, 220);
-
-    y = y + 1*dirY;
-
-    if (y >= height - size/2) {
-        dirY = dirY*-1;
-        y = y + 1*dirY;
-    }
-
-    if (y <= 0 + size/2) {
-        dirY = dirY*-1;
-        y = y + 1*dirY;
-    }
-
+  background(164, 26, 52);
+  ellipse(x, y, size);
+  fill(r, g, b);
+  move();
 }
+
+function move() {
+y = y + 1*dirY;
+  if (y >= height - size/2 ||y <= 0 + size/2) {
+    dirY = dirY*-1;
+    y = y + 1*dirY;
+      }
+}
+
 function mousePressed() {
-    x = x + 50*dirX;
-
-    if (x >= width - size/2) {
-        dirX = dirX*-1;
-        x = x + 1*dirX;
-    }
-
-    if (x <= 0 + size/2) {
-        dirX = dirX*-1;
-        x = x + 1*dirX;
-    }
+  x = x + 50*dirX;
+  
+  if (x >= width - size/2 || x <= 0 + size/2) {
+    dirX = dirX*-1;
+    x = x + 1*dirX;
+    r = r + 100
+    g = g + 100
+    b = b + 100
+      }
 }
